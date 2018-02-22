7287ad9e5f6d1ed2b013b3f248b8cbcf23483291
Allen Kennedy <allen@kennedystuff.com>
2018-02-22 17:03:15 -0400

    change to post handling for body parms

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.get('/syllabus', (req, res) => {
+app.post('/syllabus', (req, res) => {
   if (body in req && req.body !== null)
   {
     res.end(JSON.stringify(req.body, null, 2));
