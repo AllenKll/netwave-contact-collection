9387cc67b2eb9b6345ae55f9a623c1502ca2daba
Allen Kennedy <allen@kennedystuff.com>
2018-02-22 15:34:29 -0400

    Add end to send

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!').end());
 
 app.listen(3000, () => console.log(`Example app listening on port ${ PORT }!`));
