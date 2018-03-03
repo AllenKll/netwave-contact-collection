380d84416c48e34b6a38cd696cefb8097c566354
Allen Kennedy Jr <allen@kennedystuff.com>
2018-03-03 13:05:35 -0400

    Add parameter checking

--- index.js
+++ index.js
@@ -3,32 +3,40 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+var requiredFields = [
+		"name",
+		"phone",
+		"interest"
+];
+
+app.use(bodyParser.json());
+
 app.post('/syllabus', (req, res) => {
-  var buffer = "1\r\n";
-  
-  if ('body' in req && req.body !== null)
-  {
-    buffer += "2\r\n";
-    buffer += JSON.stringify(req.body, null, 2);
-    //res.end(JSON.stringify(req.body, null, 2));
-  }
-  else{
-    buffer += "3\r\n";
+  var buffer = "";
+  if ('body' in req && req.body !== null) {
+    // check for required fields
+	for (var i=0; i<requiredFields.length; ++i ){
+		if (!(requiredFields[i] in req.body)) {
+			res.status(400);
+			res.send("Missing required parameters");
+			return;
+		}
+	}
+    //buffer += JSON.stringify(req.body, null, 2);
+  } else {
     if ('body' in req){
-      buffer += "4\r\n";
-      buffer += "body in request\r\n";
-      buffer += req.body;
-      buffer += "\r\n";
+      buffer += "null body\r\n";
+    } else {
+      buffer += "no body in request\r\n";
     }
-    buffer += "5\r\n";
+    res.status(400);
   }  
   
   res.send(buffer);
 })
-.get('*', (req, res) => {
+
+app.get('*', (req, res) => {
 	res.status(404).end();
 });
 
-app.use(bodyParser.json());
-
-app.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`));
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }!`));
